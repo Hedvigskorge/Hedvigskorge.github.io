a3b0c1b8439b54ba67d8fd5a816eea82adc0306a
Hedvigskorge <42533022+Hedvigskorge@users.noreply.github.com>
2019-06-11 13:37:09 +0200

    .

---
+++ eksamen/javascript.js
@@ -0,0 +1,22 @@
+
+// Kobling til html-elementene
+const main = document.querySelector("main");
+const db = firebase.database();
+const turer = db.ref("Tur");
+
+function genererHTML(snapshot){
+  let tur = snapshot.val();
+  let nokkel = snapshot.key;
+  main.innerHTML +=`
+    <div class = "gridcontainer">
+      <h1>Terreng: ${tur.Terreng} </h1>
+      <p>Lengde: ${tur.Lengde}km </p>
+      <p>Høydeforskjell: ${tur.Høydeforskjell}m</p>
+      <a href="turDetaljer.html?id=${nokkel}">Detaljer</a>
+      ${tur.Kart}
+    </div>
+  `
+}
+
+//on er en spørring. her på db (alt sammen). Sender resultatet til funksjonen genererHTML
+turer.on("child_added",genererHTML);
